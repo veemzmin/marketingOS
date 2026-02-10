0dc3a8de9af55c92f5cf966193f971200ef182f5
veemzmin <veemzmin@users.noreply.github.com>
2026-02-10 02:11:43 -0500

    fix(prisma): bundle query engine on vercel

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    serverComponentsExternalPackages: ["@prisma/client", "prisma"],
+  },
+  outputFileTracingIncludes: {
+    "/**/*": ["./generated/prisma/**"],
+  },
   webpack: (config, { isServer }) => {
     // Fix for Prisma node:crypto imports
     if (!isServer) {
